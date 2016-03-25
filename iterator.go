81adae83409b301b631e4c8dfdf14c1f35e5a39f
Fabian Reinartz <fab.reinartz@gmail.com>
2016-03-25 13:02:16 +0100

    Implement skipIterator

--- iterator.go
+++ iterator.go
@@ -136,25 +136,99 @@ func (it *intersectIterator) seek(id uint64) (uint64, error) {
 	return it.next()
 }
 
-// listIterator implements the iterator interface on a sorted list of integers.
-type listIterator struct {
+// A skiplist iterator iterates through a list of value/pointer pairs.
+type skiplistIterator interface {
+	// seek returns the value and pointer at or before v.
+	seek(v uint64) (val, ptr uint64, err error)
+	// next returns the next value/pointer pair.
+	next() (val, ptr uint64, err error)
+}
+
+// iteratorStore allows to retrieve an iterator based on a key.
+type iteratorStore interface {
+	get(uint64) (iterator, error)
+}
+
+// skipIterator implements the iterator interface based on skiplist, which
+// allows to jump to the iterator closest to the seeked value.
+//
+// This iterator allows for speed up in seeks if the underlying data cannot
+// be searched in O(log n).
+// Ideally the skiplist is seekable in O(log n).
+type skipIterator struct {
+	skiplist  skiplistIterator
+	iterators iteratorStore
+
+	// The iterator holding the next value.
+	cur iterator
+}
+
+// seek implements the iterator interface.
+func (it *skipIterator) seek(id uint64) (uint64, error) {
+	_, ptr, err := it.skiplist.seek(id)
+	if err != nil {
+		return 0, err
+	}
+	cur, err := it.iterators.get(ptr)
+	if err != nil {
+		return 0, err
+	}
+	it.cur = cur
+
+	return it.cur.seek(id)
+}
+
+// next implements the iterator interface.
+func (it *skipIterator) next() (uint64, error) {
+	// If next was called initially.
+	// TODO(fabxc): should this just panic and initial call to seek() be required?
+	if it.cur == nil {
+		return it.seek(0)
+	}
+
+	if id, err := it.cur.next(); err == nil {
+		return id, nil
+	} else if err != io.EOF {
+		return 0, err
+	}
+	// We reached the end of the current iterator. Get the next iterator through
+	// our skiplist.
+	_, ptr, err := it.skiplist.next()
+	if err != nil {
+		// Here we return the actual io.EOF if we reached the end of the iterator
+		// retrieved from the last skiplist entry.
+		return 0, err
+	}
+	// Iterate over the next iterator.
+	cur, err := it.iterators.get(ptr)
+	if err != nil {
+		return 0, err
+	}
+	it.cur = cur
+
+	// Return the first value in the new iterator.
+	return it.cur.seek(0)
+}
+
+// plainListIterator implements the iterator interface on a sorted list of integers.
+type plainListIterator struct {
 	list list
 	pos  int
 }
 
-func newListIterator(l []uint64) *listIterator {
-	it := &listIterator{list: list(l)}
+func newPlainListIterator(l []uint64) *plainListIterator {
+	it := &plainListIterator{list: list(l)}
 	sort.Sort(it.list)
 	return it
 }
 
-func (it *listIterator) seek(id uint64) (uint64, error) {
+func (it *plainListIterator) seek(id uint64) (uint64, error) {
 	it.pos = sort.Search(it.list.Len(), func(i int) bool { return it.list[i] >= id })
 	return it.next()
 
 }
 
-func (it *listIterator) next() (uint64, error) {
+func (it *plainListIterator) next() (uint64, error) {
 	if it.pos >= it.list.Len() {
 		return 0, io.EOF
 	}
@@ -168,3 +242,47 @@ type list []uint64
 func (l list) Len() int           { return len(l) }
 func (l list) Less(i, j int) bool { return l[i] < l[j] }
 func (l list) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+
+// plainSkiplistIterator implements the skiplistIterator interface on plain
+// in-memory mapping.
+type plainSkiplistIterator struct {
+	m    map[uint64]uint64
+	keys list
+	pos  int
+}
+
+func newPlainSkiplistIterator(m map[uint64]uint64) *plainSkiplistIterator {
+	var keys list
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Sort(keys)
+
+	return &plainSkiplistIterator{
+		m:    m,
+		keys: keys,
+	}
+}
+
+// seek implements the skiplistIterator interface.
+func (it *plainSkiplistIterator) seek(id uint64) (uint64, uint64, error) {
+	pos := sort.Search(len(it.keys), func(i int) bool { return it.keys[i] >= id })
+	// The skiplist iterator points to the element at or before the last value.
+	if pos > 0 && it.keys[pos] > id {
+		it.pos = pos - 1
+	} else {
+		it.pos = pos
+	}
+	return it.next()
+
+}
+
+// next implements the skiplistIterator interface.
+func (it *plainSkiplistIterator) next() (uint64, uint64, error) {
+	if it.pos >= len(it.keys) {
+		return 0, 0, io.EOF
+	}
+	k := it.keys[it.pos]
+	it.pos++
+	return k, it.m[k], nil
+}
--- iterator_test.go
+++ iterator_test.go
@@ -19,9 +19,9 @@ func TestMultiIntersect(t *testing.T) {
 	}
 
 	for _, c := range cases {
-		i1 := newListIterator(c.a)
-		i2 := newListIterator(c.b)
-		i3 := newListIterator(c.c)
+		i1 := newPlainListIterator(c.a)
+		i2 := newPlainListIterator(c.b)
+		i3 := newPlainListIterator(c.c)
 
 		res, err := expandIterator(intersect(i1, i2, i3))
 		if err != nil {
@@ -56,8 +56,8 @@ func TestIntersectIterator(t *testing.T) {
 	}
 
 	for _, c := range cases {
-		i1 := newListIterator(c.a)
-		i2 := newListIterator(c.b)
+		i1 := newPlainListIterator(c.a)
+		i2 := newPlainListIterator(c.b)
 
 		res, err := expandIterator(&intersectIterator{i1: i1, i2: i2})
 		if err != nil {
@@ -83,9 +83,9 @@ func TestMergeIntersect(t *testing.T) {
 	}
 
 	for _, c := range cases {
-		i1 := newListIterator(c.a)
-		i2 := newListIterator(c.b)
-		i3 := newListIterator(c.c)
+		i1 := newPlainListIterator(c.a)
+		i2 := newPlainListIterator(c.b)
+		i3 := newPlainListIterator(c.c)
 
 		res, err := expandIterator(merge(i1, i2, i3))
 		if err != nil {
@@ -120,8 +120,8 @@ func TestMergeIterator(t *testing.T) {
 	}
 
 	for _, c := range cases {
-		i1 := newListIterator(c.a)
-		i2 := newListIterator(c.b)
+		i1 := newPlainListIterator(c.a)
+		i2 := newPlainListIterator(c.b)
 
 		res, err := expandIterator(&mergeIterator{i1: i1, i2: i2})
 		if err != nil {
@@ -131,4 +131,61 @@ func TestMergeIterator(t *testing.T) {
 			t.Fatalf("Expected %v but got %v", c.res, res)
 		}
 	}
+}
+
+func TestSkipIterator(t *testing.T) {
+	var cases = []struct {
+		skiplist skiplistIterator
+		its      iteratorStore
+		res      []uint64
+	}{
+		{
+			skiplist: newPlainSkiplistIterator(map[uint64]uint64{
+				5:   3,
+				50:  2,
+				500: 1,
+			}),
+			its: testIteratorStore{
+				3: newPlainListIterator(list{5, 7, 8, 9}),
+				2: newPlainListIterator(list{54, 60, 61}),
+				1: newPlainListIterator(list{1200, 1300, 100000}),
+			},
+			res: []uint64{5, 7, 8, 9, 54, 60, 61, 1200, 1300, 100000},
+		},
+		{
+			skiplist: newPlainSkiplistIterator(map[uint64]uint64{
+				0:  3,
+				50: 2,
+			}),
+			its: testIteratorStore{
+				3: newPlainListIterator(list{5, 7, 8, 9}),
+				2: newPlainListIterator(list{54, 60, 61}),
+			},
+			res: []uint64{5, 7, 8, 9, 54, 60, 61},
+		},
+	}
+
+	for _, c := range cases {
+		it := &skipIterator{
+			skiplist:  c.skiplist,
+			iterators: c.its,
+		}
+		res, err := expandIterator(it)
+		if err != nil {
+			t.Fatalf("Unexpected error", err)
+		}
+		if !reflect.DeepEqual(res, c.res) {
+			t.Fatalf("Expected %v but got %v", c.res, res)
+		}
+	}
+}
+
+type testIteratorStore map[uint64]iterator
+
+func (s testIteratorStore) get(id uint64) (iterator, error) {
+	it, ok := s[id]
+	if !ok {
+		return nil, errNotFound
+	}
+	return it, nil
 }
